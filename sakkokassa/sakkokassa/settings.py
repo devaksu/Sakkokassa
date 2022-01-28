25035a18f7ef18aaebc00d117ec54fe85b1e03ed
aleksi <aleksi.sillantaka@gmail.com>
2022-01-28 19:54:45 +0200

    removing db from git

--- sakkokassa/sakkokassa/settings.py
+++ sakkokassa/sakkokassa/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('secret.txt', 'r') as f:
+with open('sakkokassa\secret.txt', 'r') as f:
     SECRET_KEY = f.read().strip()
 
 
