42c5a474379fa9f9af16bc8f39fbd921453a9b11
Ubuntu <azureuser@Sakkokassa-cloud.tk2wycxy1aiunb0pnj00po2ksg.fx.internal.cloudapp.net>
2022-05-06 18:03:20 +0000

    Fixed up.sh

--- up.sh
+++ up.sh
@@ -4,4 +4,4 @@ sudo docker-compose exec web python manage.py makemigrations --noinput && \
 sudo docker-compose exec web python manage.py migrate --noinput && \
 sudo docker-compose exec web python manage.py collectstatic --no-input --clear && \
 sudo docker ps -a && \
-sudo docker-compose exec web python manage.py createsuperuser --username aleksi
+sudo docker-compose exec web python manage.py createsuperuser
